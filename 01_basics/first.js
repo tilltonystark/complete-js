61033d2f63156a5c9070feb82eed6806d0dbe0db
Satyam Gopal <89836460+tilltonystark@users.noreply.github.com>
2023-10-08 10:37:46 +0000

    first commit

---
+++ 01_basics/first.js
@@ -0,0 +1,11 @@
+const accountID = 45;
+let studentName = "Satyam";
+let rollNo = "2020UGCS051R";
+var branch = "CSE";
+
+if(true) {
+    let studentName = "Mantri";
+    var branch = "ECE";
+}
+
+console.table([studentName, rollNo, branch]);
